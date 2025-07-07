a1959105955bbcd5d8a9a9789362ae1da1bea24c
SebaxoxMB <146887398+SebaxoxMB@users.noreply.github.com>
2025-07-07 19:39:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app)  # Para que Google Sheets pueda hacer peticiones
 @app.route("/shotgrid_estado", methods=["POST"])
 def shotgrid_estado():
     data = request.get_json()
-    shot_codes = data.get("shot_codes", [])
+    shot_ids = data.get("shot_ids", [])
     script_name = data["script_name"]
     script_key = data["script_key"]
 
@@ -22,9 +22,14 @@ def shotgrid_estado():
         script_key
     )
 
-    filtros = [['code', 'in', shot_codes]]
-    campos = ['code', 'sg_status_list']
+    filtros = [['id', 'in', shot_ids]]
+    campos = ['id', 'code', 'sg_status_list']
     shots = sg.find("Shot", filtros, campos)
 
-    resultado = {shot['code']: shot['sg_status_list'] for shot in shots}
+    resultado = {
+        str(shot['id']): {
+            "code": shot["code"],
+            "estado": shot["sg_status_list"]
+        } for shot in shots
+    }
     return jsonify(resultado)
